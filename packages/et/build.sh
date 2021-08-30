95e673ab7ec2daa03b0e47162f72ba61b35a9857
Butta <repo@butta.fastem.com>
2021-08-30 22:56:19 +0530

    et: update from 6.0.13 to 6.1.8 and remove upstreamed patches

--- packages/et/build.sh
+++ packages/et/build.sh
@@ -2,10 +2,9 @@ TERMUX_PKG_HOMEPAGE=https://eternalterminal.dev
 TERMUX_PKG_DESCRIPTION="A remote shell that automatically reconnects without interrupting the session"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=6.0.13
-TERMUX_PKG_REVISION=2
+TERMUX_PKG_VERSION=6.1.8
 TERMUX_PKG_SRCURL=https://github.com/MisterTea/EternalTerminal/archive/et-v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=728c3a444d666897c710e33fe473d8d289263a59574451b13aa53ec3c6ac88b3
+TERMUX_PKG_SHA256=c82c62722d5e4db5b4596bcaafd4a4cf8c236968cb3782f791f5c38a62b10fb9
 TERMUX_PKG_DEPENDS="libc++, protobuf, libsodium"
 
 termux_step_pre_configure() {
